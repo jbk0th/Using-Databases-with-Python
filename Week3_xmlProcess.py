8d31a264e59117a9c26f019991c37ced5506934c
jbk0th <jbkoth51@gmail.com>
2017-08-26 09:07:24 -0700

    Progress and the DB assignment thats taken me too long and about to branch off to look for a way to do things in XML

--- Week3_xmlProcess.py
+++ Week3_xmlProcess.py
@@ -1,11 +1,32 @@
-import xml.etree.ElementTree as ET
-    #parses the xml into element form
-tree = ET.parse(r'C:\Users\Jason\Using-Databases-with-Python\tracks\Library.xml')
+import xmltodict
+#
+# import xml.etree.ElementTree as ET
+#     #parses the xml into element form
+# tree = ET.parse(r'C:\Users\Jason\Using-Databases-with-Python\tracks\Library.xml')
+#
+# root = tree.getroot()
+# itlist = root.findall('.plist/dict/dict')
+# for elem in itlist:
+#     elem[4].text()
 
-root = tree.getroot()
 
-# example
-#  itlist = root.findall('.//dict')
-# # for elem in itlist:
-# >>     for item in elem:
-#    print(item.text)
+# might be easier if could find all text with the fields that are interested in with the xml tree
+#track title, rating, len, album, artist, Genre
+with open('tracks\\Library.xml') as fd:
+    doc = xmltodict.parse(fd.read())
+artist = list() # -------
+genre = list() # ------
+album = list() # ------
+track_title = list() # ------
+length = list() # -----
+rating = list()
+count = list() # -----
+#maybe a dict of lists would be best?
+for song in doc['plist']['dict']['dict']['dict']:
+    track_title.append(song['string'][0])
+    artist.append(song['string'][1])
+    album.append(song['string'][3])
+    genre.append(song['string'][4])
+    length.append(song['integer'][2])
+    count.append(song['integer'][10])
+    rating.append(song['integer'][13])
